77dc58a94c0029856f4712625f697c9f838b5a9b
diasc <cdias07@gmail.com>
2019-02-06 01:21:59 -0200

    atualização shopping list - formulário

--- receita/my-first-app/src/app/shopping-list/shopping-list.service.ts
+++ receita/my-first-app/src/app/shopping-list/shopping-list.service.ts
@@ -1,8 +1,9 @@
 import { Ingredient } from '../shared/ingredient.model';
-import { EventEmitter } from '@angular/core';
+import { Subject } from 'rxjs/Subject';
 
 export class ShoppingListService {
-    ingredientsChanged = new EventEmitter<Ingredient[]>();
+    ingredientsChanged = new Subject<Ingredient[]>();
+    startedEditing = new Subject<number>(); // liga-se ao onEditItem()
 
   private  ingredients: Ingredient [] = [
         new Ingredient ('Apples', 5),
@@ -13,9 +14,13 @@ export class ShoppingListService {
         return this.ingredients.slice();
     }
 
+    getIngredient(index: number) {
+        return this.ingredients[index];
+    }
+
     addIngredient(ingredient: Ingredient) {
         this.ingredients.push(ingredient);
-        this.ingredientsChanged.emit(this.ingredients.slice());
+        this.ingredientsChanged.next(this.ingredients.slice());
     }
 
     addIngredients(ingredients: Ingredient[]) {
@@ -24,7 +29,17 @@ export class ShoppingListService {
       //  }
 
       this.ingredients.push(...ingredients);
-      this.ingredientsChanged.emit(this.ingredients.slice());
+      this.ingredientsChanged.next(this.ingredients.slice());
+    }
+
+    updateIngredient(index: number, newIngredient: Ingredient) {
+      this.ingredients[index] = newIngredient;
+      this.ingredientsChanged.next(this.ingredients.slice());
+    }
+
+    deleteIngredient(index: number) {
+      this.ingredients.splice(index, 1);
+      this.ingredientsChanged.next(this.ingredients.slice());
     }
 }
 
